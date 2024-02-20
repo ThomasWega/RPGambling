ee2bfc65f26dec3cf054f26af3e9dd4d1659b103
matytyma <100159609+matytyma@users.noreply.github.com>
2024-02-20 21:13:41 +0100

    Plugin initialization logic

--- src/main/java/me/wega/rpgambling/RPGambling.java
+++ src/main/java/me/wega/rpgambling/RPGambling.java
@@ -1,17 +1,24 @@
 package me.wega.rpgambling;
 
+import lombok.Getter;
+import me.wega.rpgambling.handlers.WorldGuardHandler;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class RPGambling extends JavaPlugin {
+    @Getter
+    private WorldGuardHandler worldGuard;
+    @Getter
+    private Economy vault;
+    @Getter
+    private static RPGambling instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        instance = this;
+        worldGuard = new WorldGuardHandler();
+        vault = Objects.requireNonNull(getServer().getServicesManager().getRegistration(Economy.class)).getProvider();
     }
 }
