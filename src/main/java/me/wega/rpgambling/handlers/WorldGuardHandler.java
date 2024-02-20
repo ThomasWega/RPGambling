f5ad93e4c759a39d32d23aaa712a06c30f3f22f8
matytyma <100159609+matytyma@users.noreply.github.com>
2024-02-20 21:12:08 +0100

    Added casino flag

--- src/main/java/me/wega/rpgambling/handlers/WorldGuardHandler.java
+++ src/main/java/me/wega/rpgambling/handlers/WorldGuardHandler.java
@@ -1,7 +1,26 @@
 package me.wega.rpgambling.handlers;
 
 import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 
 public class WorldGuardHandler {
+    public static StateFlag CASINO_FLAG;
     private final WorldGuard worldGuard = WorldGuard.getInstance();
+
+    public WorldGuardHandler() {
+        registerCasinoFlag();
+    }
+
+    private void registerCasinoFlag() {
+        FlagRegistry registry = worldGuard.getFlagRegistry();
+        try {
+            StateFlag flag = new StateFlag("casino", false);
+            registry.register(flag);
+            CASINO_FLAG = flag;
+        } catch (FlagConflictException e) {
+            CASINO_FLAG = (StateFlag) registry.get("casino");
+        }
+    }
 }
