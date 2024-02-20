beec2a1b7fd941f582ba4c7df7b39ef6b2f31f3e
matytyma <100159609+matytyma@users.noreply.github.com>
2024-02-20 19:19:52 +0100

    Added skeleton for WorldGuard and PAPI

---
+++ src/main/java/me/wega/rpgambling/handlers/PlaceholderHandler.java
@@ -0,0 +1,22 @@
+package me.wega.rpgambling.handlers;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.jetbrains.annotations.NotNull;
+
+public class PlaceholderHandler extends PlaceholderExpansion {
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "gb"; // TODO Dej si sem co chces
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "Wega";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+}
---
+++ src/main/java/me/wega/rpgambling/handlers/WorldGuardHandler.java
@@ -0,0 +1,7 @@
+package me.wega.rpgambling.handlers;
+
+import com.sk89q.worldguard.WorldGuard;
+
+public class WorldGuardHandler {
+    private final WorldGuard worldGuard = WorldGuard.getInstance();
+}
