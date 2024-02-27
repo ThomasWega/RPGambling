13b9a0fc6541b145f130149bf9271effca69f92f
matytyma <100159609+matytyma@users.noreply.github.com>
2024-02-27 19:20:51 +0100

    Made the `casino` flag a boolean flag instead of a state flag

--- src/main/java/me/wega/rpgambling/handlers/WorldGuardHandler.java
+++ src/main/java/me/wega/rpgambling/handlers/WorldGuardHandler.java
@@ -1,12 +1,11 @@
 package me.wega.rpgambling.handlers;
 
 import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.protection.flags.StateFlag;
-import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.BooleanFlag;
 import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 
 public class WorldGuardHandler {
-    public static StateFlag CASINO_FLAG;
+    public static BooleanFlag CASINO_FLAG;
     private final WorldGuard worldGuard = WorldGuard.getInstance();
 
     public WorldGuardHandler() {
@@ -15,12 +14,8 @@ public class WorldGuardHandler {
 
     private void registerCasinoFlag() {
         FlagRegistry registry = worldGuard.getFlagRegistry();
-        try {
-            StateFlag flag = new StateFlag("casino", false);
-            registry.register(flag);
-            CASINO_FLAG = flag;
-        } catch (FlagConflictException e) {
-            CASINO_FLAG = (StateFlag) registry.get("casino");
-        }
+        BooleanFlag flag = new BooleanFlag("casino");
+        registry.register(flag);
+        CASINO_FLAG = flag;
     }
 }
