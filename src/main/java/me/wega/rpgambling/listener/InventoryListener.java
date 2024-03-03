3a283b04a5edbe7ac1f53d7b5db771be63fc9815
matytyma <100159609+matytyma@users.noreply.github.com>
2024-03-03 21:39:31 +0100

    Implement `InventoryListener`

--- src/main/java/me/wega/rpgambling/listener/InventoryListener.java
+++ src/main/java/me/wega/rpgambling/listener/InventoryListener.java
@@ -1,18 +1,26 @@
 package me.wega.rpgambling.listener;
 
+import me.wega.rpgambling.gui.InventoryData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.InventoryHolder;
 
 public class InventoryListener implements Listener {
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
-
+        InventoryHolder holder = event.getInventory().getHolder();
+        if (!(holder instanceof InventoryData)) return;
+        ((InventoryData) holder).onClose();
     }
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-
+        if (event.getView().getTopInventory() != event.getClickedInventory()) return;
+        InventoryHolder holder = event.getInventory().getHolder();
+        if (!(holder instanceof InventoryData)) return;
+        ((InventoryData) holder).onClick(event.getSlot());
+        event.setCancelled(true);
     }
 }
