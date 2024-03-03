457b01a902296c0b5e32e6926b369f2ac46f8b73
matytyma <100159609+matytyma@users.noreply.github.com>
2024-03-03 21:15:29 +0100

    `InventoryListener` skeleton

---
+++ src/main/java/me/wega/rpgambling/listener/InventoryListener.java
@@ -0,0 +1,18 @@
+package me.wega.rpgambling.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+public class InventoryListener implements Listener {
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+
+    }
+}
