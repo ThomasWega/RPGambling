ddec1cb34c0471ac8621843a3ed238270bbcd068
matytyma <100159609+matytyma@users.noreply.github.com>
2024-03-04 20:14:38 +0100

    Added inventory reopening

--- src/main/java/me/wega/rpgambling/listener/InventoryListener.java
+++ src/main/java/me/wega/rpgambling/listener/InventoryListener.java
@@ -5,14 +5,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 public class InventoryListener implements Listener {
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
+        Inventory inventory = event.getInventory();
         InventoryHolder holder = event.getInventory().getHolder();
         if (!(holder instanceof InventoryData)) return;
-        ((InventoryData) holder).onClose();
+        boolean close = ((InventoryData) holder).onClose();
+        if (event.getReason() != InventoryCloseEvent.Reason.PLAYER || !close) return;
+        event.getPlayer().openInventory(inventory);
     }
 
     @EventHandler
